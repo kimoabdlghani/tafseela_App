eb84a13262bc23e3e8fc4b115972fa65815f0a4b
kimoabdlghani <karrem763@gmail.com>
2026-08-13 19:10:01 +0300

    Measurment Model

---
+++ Backend/ECommerce/ECommerce.Domain/Entities/Measurement.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Entities
+{
+    public class Measurement : BaseEntity, ISoftDeletable
+    {
+        public string Unit { get; set; }
+        public decimal Length { get; set; }
+
+        public decimal Width { get; set; }
+
+        public decimal Height { get; set; }
+
+        public Product Product { get; set; } = null!;
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
--- Backend/ECommerce/ECommerce.Domain/Entities/Product.cs
+++ Backend/ECommerce/ECommerce.Domain/Entities/Product.cs
@@ -13,6 +13,8 @@ namespace ECommerce.Domain.Entities
         public DateTime? DeletedAt { get; set; }
         public Category Category { get; set; } = null!;
         public Brand? Brand { get; set; }
+
+        public ICollection<Measurement> Measurements { get; set; } = [];
         public ICollection<ProductImage> Images { get; set; } = [];
         public ICollection<ProductVariant> Variants { get; set; } = [];
         public ICollection<Review> Reviews { get; set; } = [];
